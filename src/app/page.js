578a0911c0fd37a86e80de4d07f90c87e621d627
Maxougit <84478576+Maxougit@users.noreply.github.com>
2024-01-15 22:16:45 +0100

    Update styles and components

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-"use client"
-import React, { useState } from 'react';
-import WindowsNavBar from '@/components/NavBar/WindowsNavBar';
-import StartMenu from '@/components/StartMenu/StartMenu'; // Assure-toi d'importer le composant StartMenu
-import styles from './Page.module.css';
-import Window from '@/components/Windows/Windows';
+"use client";
+import React, { useState } from "react";
+import WindowsNavBar from "@/components/NavBar/WindowsNavBar";
+import StartMenu from "@/components/StartMenu/StartMenu"; // Assure-toi d'importer le composant StartMenu
+import styles from "./Page.module.css";
+import Window from "@/components/Windows/Windows";
 
 export default function Home() {
   const [showStartMenu, setShowStartMenu] = useState(false);
-  const [windows, setWindows] = useState([]); 
+  const [windows, setWindows] = useState([]);
   const [nextId, setNextId] = useState(0); // Ajout d'un compteur pour les identifiants
 
   const toggleStartMenu = () => {
@@ -22,9 +22,8 @@ export default function Home() {
   return (
     <div className={styles.mainContainer}>
       <main className={styles.content}>
-        <h1 className="text-4xl font-bold text-center">
-          Maxence OS
-        </h1>
+        <h1 className="text-4xl font-bold text-center">Maxence OS</h1>
+        <p className="text-center">Site en construction</p>
       </main>
       {windows.map((window) => (
         <Window
@@ -37,7 +36,10 @@ export default function Home() {
           {window.content}
         </Window>
       ))}
-      <WindowsNavBar onToggleStartMenu={toggleStartMenu} openWindow={openWindow} />
+      <WindowsNavBar
+        onToggleStartMenu={toggleStartMenu}
+        openWindow={openWindow}
+      />
       {showStartMenu && <StartMenu />}
     </div>
   );
